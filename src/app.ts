c89b2abc194595bd0005d48dc16226cc59dbff8a
gyoungsubLee <dltjq2323@gmail.com>
2025-07-06 16:05:02 +0900

    :

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,36 @@
 import express from "express";
+import { Cat, CatType } from "./app.module";
 
-const app: express.Express = express();
+const app: express.Application = express();
 
-const port: number = 8000;
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  console.log("this is som middleware");
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
+  res.send({ cats: Cat });
 });
 
-app.get("/test", (req: express.Request, res: express.Response) => {
-  res.send({ Endpoint: "/test", Method: "GET" });
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/cats/som", (req, res) => {
+  res.send({ blue: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on...");
 });
