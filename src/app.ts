130e880394c739744e9fcdce5c4e1bef3092ec14
gyoungsubLee <dltjq2323@gmail.com>
2025-07-06 15:43:13 +0900

    feat: setup express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("hello world!!");
+import express from "express";
+
+const app: express.Express = express();
+
+const port: number = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/test", (req: express.Request, res: express.Response) => {
+  res.send({ Endpoint: "/test", Method: "GET" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
