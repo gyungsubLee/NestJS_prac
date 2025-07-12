274203d0a68ee62d211e6e0f91e53b675d719d5e
gyoungsubLee <dltjq2323@gmail.com>
2025-07-12 17:34:10 +0900

    feat: Add http error filter

---
+++ src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpErrorFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const errorResponse = exception.getResponse();
+
+    response.status(status).json({
+      success: false,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+      ...(typeof errorResponse === 'string'
+        ? { message: errorResponse }
+        : errorResponse),
+    });
+  }
+}
